3501f5cd228ae87005c822bc912ccb175f183bb6
Dmitrij Petters <dpetters@mit.edu>
2012-04-30 16:55:49 -0400

    about fix

--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from core.decorators import render_to
 from core.models import ExecMember
 
-@render_to("about_us.html")
-def about_us(request, extra_context=None):
+@render_to("about.html")
+def about(request, extra_context=None):
     context = {'current_exec_members':ExecMember.objects.current()}
     return context
 

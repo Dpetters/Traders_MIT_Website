9ace60c6f5c4fb897196cd2745518a703f56861b
Dmitrij Petters <dpetters@mit.edu>
2012-04-30 21:36:23 -0400

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 import os
 
-from fabric.api import local, settings as fabric_settings
+from fabric.api import local
 from fabric.contrib import django as fabric_django
 
 fabric_django.settings_module('settings')
@@ -11,7 +11,6 @@ DATA_MODELS = {
     'core': ['course', 'graduationyear', 'boardmember', 'execmember'],
     'event': ['event']
 }
-LOCAL_DATA_MODELS = {}
 
 def migrate():
     local("python manage.py migrate --all --no-initial-data")
@@ -22,15 +21,11 @@ def load_data():
                 
 def commit_data():
     local("python manage.py dumpdata flatpages sites auth.group auth.user --indent=1 > ./initial_data.json")
-    directories = ""
     for app in DATA_MODELS:
         model_labels = []
         fixtures_dir = "./%s/fixtures" % (app)
         if not os.path.exists(fixtures_dir):
             os.makedirs(fixtures_dir)
-        directories += "%s/* " % fixtures_dir
-        with fabric_settings(warn_only=True):
-            local("mkdir %s" % (fixtures_dir))
         for model in DATA_MODELS[app]:
             model_labels.append("%s.%s" % (app, model))
         local("python manage.py dumpdata %s --indent=1 > %s/initial_data.json" % (" ".join(model_labels), fixtures_dir))

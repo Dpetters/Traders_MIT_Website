6ce1e93faa3901f3d05c555790445428fa4ea184
Dmitrij <Dpetters91@gmail.com>
2012-09-09 15:45:19 -0400

    reorder upcoming events

--- event/views.py
+++ event/views.py
@@ -5,7 +5,7 @@ from event.models import Event
 
 @render_to("events_upcoming.html")
 def events_upcoming(request, extra_context=None):
-    context = {'upcoming_events' : filter(lambda x: not x.is_past(), Event.objects.all())}
+    context = {'upcoming_events' : filter(lambda x: not x.is_past(), Event.objects.all().order_by("end_datetime"))}
     return context
 
 @render_to("events_past.html")
